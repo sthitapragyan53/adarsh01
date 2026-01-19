3cbb68fad084f5d746618e81c341ab323ed4e247
sthita <sthitapragyan8353@gmail.com>
2026-01-19 14:27:09 +0530

    fixed routing in  in main file

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -209,7 +209,7 @@ export default function Dashboard() {
 
             <button
               className="action-btn roadmap"
-              onClick={() => navigate("/roadMap")}
+              onClick={() => navigate("/roadmaps")}
             >
               🧭 Roadmap
               <span>Step-by-step plan</span>
