661bc4f9a2481d7fd9a660af1f64d0d7b9910eb0
sthita <sthitapragyan8353@gmail.com>
2026-01-20 23:04:38 +0530

    corrected typo in jee main chapter details

--- frontend/src/pages/jeeMains/JeeMainChapterDetail.jsx
+++ frontend/src/pages/jeeMains/JeeMainChapterDetail.jsx
@@ -1,7 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
-import "./JeeMainsChapterDetail.css";
+import "./JeeMainChapterDetail.css";
 
 export default function JeeMainChapterDetail() {
   const navigate = useNavigate();
